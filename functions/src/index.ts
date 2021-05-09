644dcfc4fe3d0df5bcd281954d122d6729c06e62
digi0ps <sriru1998@gmail.com>
2021-05-09 19:40:54 +0530

    Fix routes.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,16 +6,14 @@ admin.initializeApp({ projectId: "fractalhub-612ee" });
 // handlers
 import * as recur from "./handlers/dispatcher";
 import * as user from "./handlers/user";
-import * as check from "./handlers/checkin";
 
 export const ping = https.onCall((data, context) => {
   return "pong";
 });
 
 // recurs
-export const dispatch = recur.dispatch;
+export const fetchCommits = recur.fetchCommits;
+export const pushCommits = recur.pushCommits;
 
-// checins
-export const checkInRecur = check.inRecur;
 // user
 export const onUserCreate = user.create;
