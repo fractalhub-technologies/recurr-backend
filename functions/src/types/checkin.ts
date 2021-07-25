f62e9c3666c87e9cc26eb269c178a947db42f940
digi0ps <sriru1998@gmail.com>
2021-07-25 20:56:04 +0530

    Delete unnecessary code.

--- functions/src/types/checkin.ts
+++
@@ -1,4 +0,0 @@
-export interface CheckInParams {
-  recurId: string;
-  timestamp: string;
-}
--- functions/src/types/recur.ts
+++ functions/src/types/recur.ts
@@ -1,43 +1,8 @@
 // Model Schema on document
-export interface Recur {
-  title: string;
-  duration: number;
-  type?: "personal" | "group";
-}
-
 export interface Response {
   success: boolean;
   error?: string;
   data?: object;
-}
-
-export interface CreateRecurParams {
-  title: string;
-  duration: number;
-}
-
-export interface CreateRecurResponse extends Response {
-  data: {
-    id: string;
-  };
-}
-
-export interface ListRecurResponse extends Response {
-  data: FirebaseFirestore.DocumentData[];
-}
-
-export interface DeleteRecurParams {
-  id: number;
-}
-
-export interface DeleteRecurResponse extends Response {}
-
-export interface UpdateRecurParams {
-  id: string;
-  updateData: {
-    title?: string;
-    duration?: number;
-  };
 }
 
 export interface Commit {
